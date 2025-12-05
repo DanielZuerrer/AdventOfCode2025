3b3d3ff91f2ef14700c0cd81095921c0b63de47d
Daniel Zürrer <daniel.zuerrer@gmail.com>
2025-12-05 21:20:22 +0100

    day 5

--- AdventOfCode2025/Day5.cs
+++ AdventOfCode2025/Day5.cs
@@ -6,13 +6,54 @@ public static class Day5
     {
         var input = File.ReadAllLines("Inputs/day5.txt");
 
-        return string.Empty;
+        var divider = input.IndexOf(string.Empty);
+
+        var ranges = input[..divider].Select(r => r.Split('-').Select(long.Parse).ToArray()).ToArray();
+
+        var ids = input[(divider + 1)..].Select(long.Parse).ToArray();
+
+        var freshCount = ids.Count(id => ranges.Any(range => id >= range[0] && id <= range[1]));
+
+        return freshCount.ToString();
     }
 
     public static string B()
     {
         var input = File.ReadAllLines("Inputs/day5.txt");
 
-        return string.Empty;
+        var divider = input.IndexOf(string.Empty);
+
+        var ranges = input[..divider].Select(r => r.Split('-').Select(long.Parse).ToArray()).ToArray();
+
+        var nonOverlappingRanges = ranges.OrderBy(range => range[0])
+            .Aggregate(new List<long[]>(), (nonOverlappingRanges, currentRange) =>
+            {
+                // first range
+                if (nonOverlappingRanges.Count == 0)
+                {
+                    nonOverlappingRanges.Add(currentRange);
+                    return nonOverlappingRanges;
+                }
+                
+                // range does not overlap
+                if (nonOverlappingRanges.Last()[1] < currentRange[0])
+                {
+                    nonOverlappingRanges.Add(currentRange);
+                    return nonOverlappingRanges;
+                }
+                
+                // range does overlap
+                if (nonOverlappingRanges.Last()[1] < currentRange[1])
+                {
+                    nonOverlappingRanges.Last()[1] = currentRange[1];
+                    return nonOverlappingRanges;
+                }
+                
+                return nonOverlappingRanges;
+            });
+        
+        var freshIdCount = nonOverlappingRanges.Sum(range => range[1] - range[0] + 1);
+
+        return freshIdCount.ToString();
     }
 }
